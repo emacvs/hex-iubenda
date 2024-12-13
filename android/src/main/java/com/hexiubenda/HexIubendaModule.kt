9aa1f0f95dce1e6e44cf524b8d95ae613670fe9d
emacvs <emanuele.cavestri@outlook.com>
2024-12-13 18:37:33 +0100

    installazione librerie android

--- android/src/main/java/com/hexiubenda/HexIubendaModule.kt
+++ android/src/main/java/com/hexiubenda/HexIubendaModule.kt
@@ -4,6 +4,9 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.ReadableMap;
+import com.iubenda.iab.IubendaCMP;
+import com.iubenda.iab.IubendaCMPConfig;
 
 class HexIubendaModule(reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
@@ -18,6 +21,51 @@ class HexIubendaModule(reactContext: ReactApplicationContext) :
   fun multiply(a: Double, b: Double, promise: Promise) {
     promise.resolve(a * b)
   }
+
+@ReactMethod
+    public void initialize(ReadableMap configMap) {
+        final ReactApplicationContext context = getReactApplicationContext();
+        IubendaCMPConfig.Builder builder = IubendaCMPConfig.builder();
+        if(hasValidKey("gdprEnabled",configMap))
+            builder.gdprEnabled(configMap.getBoolean("gdprEnabled"));
+        if(hasValidKey("forceConsent",configMap))
+            builder.forceConsent(configMap.getBoolean("forceConsent"));
+        if(hasValidKey("googleAds",configMap))
+            builder.googleAds(configMap.getBoolean("googleAds"));
+        if(hasValidKey("siteId",configMap))
+            builder.siteId(configMap.getString("siteId"));
+        if(hasValidKey("cookiePolicyId",configMap))
+            builder.cookiePolicyId(configMap.getString("cookiePolicyId"));
+        if(hasValidKey("cssContent",configMap))
+            builder.cssContent(configMap.getString("cssContent"));
+        if(hasValidKey("jsonContent",configMap))
+            builder.jsonContent(configMap.getString("jsonContent"));
+        if(hasValidKey("cssUrl",configMap))
+            builder.cssUrl(configMap.getString("cssUrl"));
+        if(hasValidKey("applyStyles",configMap))
+            builder.applyStyles(configMap.getBoolean("applyStyles"));
+        if(hasValidKey("acceptIfDismissed",configMap))
+            builder.acceptIfDismissed(configMap.getBoolean("acceptIfDismissed"));
+        if(hasValidKey("skipNoticeWhenOffline",configMap))
+            builder.skipNoticeWhenOffline(configMap.getBoolean("skipNoticeWhenOffline"));
+        if(hasValidKey("preventDismissWhenLoaded",configMap))
+            builder.preventDismissWhenLoaded(configMap.getBoolean("preventDismissWhenLoaded"));
+        if(hasValidKey("csVersion",configMap))
+            builder.csVersion(configMap.getString("csVersion"));
+        if(hasValidKey("proxyUrl",configMap))
+            builder.proxyUrl(configMap.getString("proxyUrl"));
+        if(hasValidKey("portraitWidth",configMap))
+            builder.portraitWidth(configMap.getInt("portraitWidth"));
+        if(hasValidKey("portraitHeight",configMap))
+            builder.portraitHeight(configMap.getInt("portraitHeight"));
+        if(hasValidKey("landscapeWidth",configMap))
+            builder.landscapeWidth(configMap.getInt("landscapeWidth"));
+        if(hasValidKey("landscapeHeight",configMap))
+            builder.landscapeHeight(configMap.getInt("landscapeHeight"));
+        IubendaCMPConfig config = builder.build();
+        IubendaCMP.initialize(context, config);
+    }
+
 
   companion object {
     const val NAME = "HexIubenda"
